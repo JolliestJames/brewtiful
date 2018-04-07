4f72b2ad4a63898c4e8f5f406a4de2c691448c8b
ToBeAmped <martinja@oregonstate.edu>
2018-04-07 10:12:34 -0700

    Update brewery_spec.rb unit test names to match the api

--- spec/models/brewery_spec.rb
+++ spec/models/brewery_spec.rb
@@ -1,12 +1,12 @@
 require "rails_helper"
 
 RSpec.describe Brewery do
-  it "considers a brewery with no beers to be out of business" do
+  it "considers a brewery with no beers to be out of stock" do
     brewery = Brewery.new
     expect(brewery.out_of_stock?).to be_truthy
   end
 
-  it "knows that a brewery with beers is not out of business" do
+  it "knows that a brewery with beers is not out of stock" do
     brewery = Brewery.new
     brew = Brew.new
     brewery.brews << brew
