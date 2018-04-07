28aafd12bf9d74435c486db56a4d6887b96cb6ad
ToBeAmped <martinja@oregonstate.edu>
2018-04-07 10:24:13 -0700

    Refactor brewery_spec.rb

--- spec/models/brewery_spec.rb
+++ spec/models/brewery_spec.rb
@@ -1,15 +1,16 @@
 require "rails_helper"
 
 RSpec.describe Brewery do
+  let(:brewery) { Brewery.new }
+  let(:brew) { Brew.new }
+
   it "considers a brewery with no beers to be out of stock" do
-    brewery = Brewery.new
     expect(brewery.out_of_stock?).to be_truthy
   end
 
   it "knows that a brewery with beers is not out of stock" do
-    brewery = Brewery.new
-    brew = Brew.new
     brewery.brews << brew
     expect(brewery.out_of_stock?).to be_falsy
   end
+  
 end 
