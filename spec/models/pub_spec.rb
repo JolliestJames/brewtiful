3b7ea39764890effa0750a1903be6c1769bd1225
ToBeAmped <martinja@oregonstate.edu>
2018-04-11 12:47:53 -0700

    Add one unit test to pub_spec.rb

--- spec/models/pub_spec.rb
+++ spec/models/pub_spec.rb
@@ -2,5 +2,11 @@ require "rails_helper"
 
 RSpec.describe Pub do
   let(:pub) { Pub.new }
-  
+  let(:brew) { Brew.new }
+
+  it "knows that a pub must have at least one brew on tap" do
+    pub.tap_list << brew
+    expect(pub.has_empty_tap_list?).to be_falsy
+  end
+
 end
