683ca8be2036ed18c5a08212c375533032a63e8d
ToBeAmped <martinja@oregonstate.edu>
2018-04-11 19:26:15 -0700

    Refactor brewery_spec.rb to test for brewery inheritance from business class

--- spec/models/brewery_spec.rb
+++ spec/models/brewery_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe Brewery do
   let(:brewery) { Brewery.new("Bend", "Deschutes") }
   let(:brew) { Brew.new }
 
+  it "knows that a brewery is a business" do
+    expect(Pub).to be < Business
+  end
+
   it "considers a brewery with no beers to be out of stock" do
     expect(brewery).to be_out_of_stock
   end
@@ -11,14 +15,6 @@ RSpec.describe Brewery do
   it "knows that a brewery with beers is not out of stock" do
     brewery.brews << brew
     expect(brewery).not_to be_out_of_stock
-  end
-
-  it "knows that a brewery has a location" do
-    expect(brewery.has_location?).to be_truthy
-  end
-
-  it "knows that a brewery has a name" do
-    expect(brewery.has_name?).to be_truthy
   end
 
 end 
