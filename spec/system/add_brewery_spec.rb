77da3cc11cd26dc8d1e5f9cb993db310809e0e10
ToBeAmped <martinja@oregonstate.edu>
2018-04-16 15:01:46 -0700

    Add unhappy path feature test

--- spec/system/add_brewery_spec.rb
+++ spec/system/add_brewery_spec.rb
@@ -1,6 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "adding a brewery", type: :system do
+
   it "allows a user to create a brewery" do
     visit new_brewery_path
     fill_in "Name", with: "Deschutes Brewery"
@@ -13,4 +14,13 @@ RSpec.describe "adding a brewery", type: :system do
     expect(page).to have_selector(
       "#brewery_#{@brewery.id} .location", text: "Bend, OR")
   end
+
+  it "does not allow a user to create a brewery without a name" do
+    visit new_brewery_path
+    fill_in "Name", with: ""
+    fill_in "Location", with: "Bend, OR"
+    click_on("Create Brewery")
+    expect(page).to have_selector(".new_brewery")
+  end
+
 end
