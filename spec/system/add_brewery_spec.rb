683de8fc4d929f9deb5254c990317259de38ce4c
ToBeAmped <martinja@oregonstate.edu>
2018-04-16 13:36:45 -0700

    Refactor add_brewery_spec, update breweries index

--- spec/system/add_brewery_spec.rb
+++ spec/system/add_brewery_spec.rb
@@ -7,7 +7,10 @@ RSpec.describe "adding a brewery", type: :system do
     fill_in "Location", with: "Bend, OR"
     click_on("Create Brewery")
     visit breweries_path
-    expect(page).to have_content("Deschutes Brewery")
-    expect(page).to have_content("Bend, OR")
+    @brewery = Brewery.find_by(name: "Deschutes Brewery")
+    expect(page).to have_selector(
+      "#brewery_#{@brewery.id} .name", text: "Deschutes Brewery")
+    expect(page).to have_selector(
+      "#brewery_#{@brewery.id} .location", text: "Bend, OR")
   end
 end
