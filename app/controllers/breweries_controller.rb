f6d64b9725e435bf352e37a516f31640b32047f9
ToBeAmped <martinja@oregonstate.edu>
2018-04-16 13:10:39 -0700

    Add index and create methods to breweries_controller

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -2,4 +2,16 @@ class BreweriesController < ApplicationController
   def new
     @brewery = Brewery.new
   end
+
+  def index
+    @breweries = Brewery.all
+  end
+
+  def create
+    @workflow = CreatesBrewery.new(
+      name: params[:brewery][:name],
+      location: params[:brewery][:location])
+    @workflow.create
+    redirect_to breweries_path
+  end
 end
