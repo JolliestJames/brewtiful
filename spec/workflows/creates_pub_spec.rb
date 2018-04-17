08381c285476cfc951d3fd65b71290fbba99b917
ToBeAmped <martinja@oregonstate.edu>
2018-04-17 00:26:11 -0700

    Resolve failure in pub feature test

--- spec/workflows/creates_pub_spec.rb
+++ spec/workflows/creates_pub_spec.rb
@@ -2,13 +2,12 @@ require "rails_helper"
 
 RSpec.describe CreatesPub do
 
-  # let(:creator) { CreatesPub.new(
-  #   name: "Brother John's West Side", location: "Bend, OR"
-  # )}
+  let(:creator) { CreatesPub.new(
+    name: "Brother John's West Side", location: "Bend, OR"
+  )}
 
   describe "initialization" do
     it "creates a pub given a name" do
-      creator = Pub.new(name: "Brother John's West Side", location: "Bend, OR")
       creator.build
       expect(creator.pub.name).to eq("Brother John's West Side")
     end
