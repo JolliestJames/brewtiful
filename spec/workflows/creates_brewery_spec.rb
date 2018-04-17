a37500cd1d0b6af766b79d73b4cd7a9b955e4199
ToBeAmped <martinja@oregonstate.edu>
2018-04-16 17:31:07 -0700

    Add describe "failure cases" block, refactor creates_brewery_spec

--- spec/workflows/creates_brewery_spec.rb
+++ spec/workflows/creates_brewery_spec.rb
@@ -2,17 +2,27 @@ require "rails_helper"
 
 RSpec.describe CreatesBrewery do
 
-  let(:creator) { CreatesProject.new(
-    name: "Deschutes Brewery", location: "Bend,OR"
+  let(:creator) { CreatesBrewery.new(
+    name: name, location: location
   )}
 
   describe "initialization" do
+    let(:name) {"Deschutes Brewery"}
+    let(:location) {"Bend, OR"}
     it "creates a brewery given a name and a location" do
-      creator = CreatesBrewery.new(name: "Deschutes Brewery", location: "Bend, OR")
       creator.build
       expect(creator.brewery.name).to eq("Deschutes Brewery")
       expect(creator.brewery.location).to eq("Bend, OR")
     end
   end
-  
+
+  describe "failure cases" do
+    let(:name) {""}
+    let(:location) {"Bend, OR"}
+    it "fails when trying to save a brewery with no name" do
+      creator.create
+      expect(creator).not_to be_a_success
+    end
+  end
+
 end
