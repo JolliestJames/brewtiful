32485cc95b2c6675a4c85bb0ec04f2cca0055bc5
joshuajmurray <joshuajmurray@gmail.com>
2018-04-17 03:04:32 -0700

    Add test for failure in add_pub_spec

--- spec/system/add_pub_spec.rb
+++ spec/system/add_pub_spec.rb
@@ -10,4 +10,12 @@ RSpec.describe "adding a pub", type: :system do
     expect(page).to have_content("Brother John's West Side")
     expect(page).to have_content("Bend, OR")
   end
+  it "does not allow a user to create a pub without a name" do
+    visit new_pub_path
+    fill_in "Name", with: ""
+    fill_in "Location", with: "Bend, OR"
+    click_on("Create Pub")
+    visit pubs_path
+    expect(page).to have_selector(".new_pub")
+  end
 end
