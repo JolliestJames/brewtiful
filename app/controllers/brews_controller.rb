9f708b70771a16a92d7ac8ca09c023003b95d91e
ToBeAmped <martinja@oregonstate.edu>
2018-04-21 19:44:10 -0700

    Implement create method in brews controller

--- app/controllers/brews_controller.rb
+++ app/controllers/brews_controller.rb
@@ -9,7 +9,18 @@ class BrewsController < ApplicationController
   end
 
   def create
-
+    @workflow = CreatesBrew.new(
+      name: params[:brew][:name],
+      abv: params[:brew][:abv],
+      ibus: params[:brew][:ibus],
+      classification: params[:brew][:classification])
+    @workflow.create
+    if @workflow.success?
+      redirect_to brews_path
+    else
+      @brew = @workflow.brew
+      render :new
+    end
   end
 
 end
