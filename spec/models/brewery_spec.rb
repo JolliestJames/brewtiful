8cddf5daa88984123ef0b2e788d1c361aa3662bd
ToBeAmped <martinja@oregonstate.edu>
2018-04-25 15:40:22 -0700

    Add test for most bitter brew

--- spec/models/brewery_spec.rb
+++ spec/models/brewery_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe Brewery do
   let(:brewery) { Brewery.new(name: "Deschutes Brewery", location: "Bend") }
-  let(:brew) { Brew.new(name: "The Abyss", abv: 0.11, ibus: 68, classification: "Imperial Stout") }
+  let(:brew) { Brew.new(name: "The Abyss", abv: 0.11, ibus: 30, classification: "Imperial Stout") }
   let(:brew2) { Brew.new(name: "Mirror Pond Pale Ale", abv: 0.05, ibus: 40, classification: "Imperial Stout") }
   let(:brew3) { Brew.new(name: "Red Chair Pale Ale", abv: 0.062, ibus: 60, classification: "North West Pale Ale") }
 
@@ -21,6 +21,13 @@ RSpec.describe Brewery do
 
   it "knows that a brewery has a name" do
     expect(brewery.name).not_to be_nil
+  end
+
+  it "knows that a brewery knows which of its brews is most bitter" do
+    brewery.brews << brew
+    brewery.brews << brew2
+    brewery.brews << brew3
+    expect(brewery.most_bitter_brew).to eq(brew3)
   end
 
 end 
