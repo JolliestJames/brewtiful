d936612dd3ed4b7b45c4ce0ef18adbf064ab4579
ToBeAmped <martinja@oregonstate.edu>
2018-04-26 09:29:26 -0700

    Refactor brewery methods

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -8,27 +8,19 @@ class Brewery < ApplicationRecord
   end
 
   def most_bitter_brew
-    most_bitter_brew = nil
-    most_bitter_brew = self.brews.max_by { |brew| brew.ibus }
-    most_bitter_brew
+    self.brews.max_by { |brew| brew.ibus }
   end
 
   def least_bitter_brew
-    least_bitter_brew = nil
-    least_bitter_brew = self.brews.min_by { |brew| brew.ibus }
-    least_bitter_brew
+    self.brews.min_by { |brew| brew.ibus }
   end
 
   def strongest_brew
-    strongest_brew = nil
-    strongest_brew = self.brews.max_by { |brew| brew.abv }
-    strongest_brew
+    self.brews.max_by { |brew| brew.abv }
   end
 
   def weakest_brew
-    least_abv_brew = nil
     least_abv_brew = self.brews.min_by { |brew| brew.abv }
-    least_abv_brew
   end
 
 end
