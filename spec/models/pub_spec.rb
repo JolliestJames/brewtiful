55f4fb689f481827cd7dfe85b6163cccecd8e285
joshuajmurray <joshuajmurray@gmail.com>
2018-04-26 11:45:54 -0700

    Add pub2 double for mock

--- spec/models/pub_spec.rb
+++ spec/models/pub_spec.rb
@@ -48,10 +48,11 @@ RSpec.describe Pub do
   end
 
   it "mocks an object" do
-    expect(pub).to receive(:name).and_return("Pub stub")
-    expect(pub).to receive(:location).and_return("Location stub")
-    expect(pub.name).to eq("Pub stub")
-    expect(pub.location).to eq("Location stub")
+    pub2 = double()
+    expect(pub2).to receive(:name).and_return("Pub stub")
+    expect(pub2).to receive(:location).and_return("Location stub")
+    expect(pub2.name).to eq("Pub stub")
+    expect(pub2.location).to eq("Location stub")
   end
 
 end
