37e3e3e0dba8dd3aba3855d629647006a6ac1bb4
ToBeAmped <martinja@oregonstate.edu>
2018-05-12 13:09:18 -0700

    Add test doubles describe block

--- spec/models/brew_spec.rb
+++ spec/models/brew_spec.rb
@@ -33,4 +33,42 @@ RSpec.describe Brew do
     expect(brew.perform(fake_big_brew_dependency)).to eq(42)
   end
   
+  describe "test doubles" do
+
+    it "stubs an object" do
+      expect(brew).to receive(:name).and_return("Brewtiful")
+      expect(brew.name).to eq("Brewtiful")
+    end
+
+    #Is the difference between the mock and the stub simply that we must mock every method?
+
+    it "mocks an object" do
+      expect(brew).to receive(:name).and_return("Brew stub")
+      expect(brew).to receive(:abv).and_return(0.12)
+      expect(brew).to receive(:ibus).and_return(100.00)
+      expect(brew).to receive(:classification).and_return("Classification stub")
+      expect(brew.name).to eq("Brew stub")
+      expect(brew.abv).to eq(0.12)
+      expect(brew.ibus).to eq(100)
+      expect(brew.classification).to eq("Classification stub")
+    end
+
+    it "stubs the class" do
+      allow(Brew).to receive(:find).and_return(
+        Brew.new(
+          name: "Stubbed brew",
+          abv: 0.99,
+          ibus: 1000.00,
+          classification: "Stubbed classification"
+        )
+      )
+      brew = Brew.find(1)
+      expect(brew.name).to eq("Stubbed brew")
+      expect(brew.abv).to eq(0.99)
+      expect(brew.ibus).to eq(1000.00)
+      expect(brew.classification).to eq("Stubbed classification")
+    end
+
+  end
+
 end
